9ebe61e3f55ec4d22ae0351a65fe630a5c258169
sagrawal1234 <70326310+sagrawal1234@users.noreply.github.com>
2021-08-24 18:24:16 +0530

    Add files via upload

--- guessingNumberGame.py
+++ guessingNumberGame.py
@@ -1,25 +1,43 @@
 import random
 
-print(" Welcome to Number Guessing Game")
-print("Guess Any number from 1 to 9 ")
+print("Number guessing game")
 
-number = random.randint(1,9)
-chances = 1
+# randint function to generate the random number between 1 to 9
+number = random.randint(1, 9)
 
+# number of chances to be given to the user to guess the number
+# or it is the inputs given by user into input box here number of chances are 5
+chances = 0
+
+print("Guess a number (between 1 and 9):")
+
+# While loop to count the number of chances
 while chances < 5:
-    guess = input(" Guess correct number : ")
-    chances = chances + 1
+
+    # Enter a number between 1 to 9
+    guess = int(input("Enter your guess:- "))
+
+    # Compare the user entered number with the number to be guessed
+
     if guess == number:
-     print("Congratulations You have WON THE GAME !!! ")
-     break
+        # if number entered by user is same as the generated
+        # number by randint function then break from loop using loop
+        # control statement "break"
+        print("Congratulation YOU WON!!!")
+        break
 
-if not chances <5 :
-    print("You have lost the game !!, The number is :",number)
+    # Check if the user entered number is smaller than the generated number
+    elif guess < number:
+        print("Your guess was too low: Guess a number higher than", guess)
+
+    # The user entered number is greater than the generated number
+    else:
+        print("Your guess was too high: Guess a number lower than", guess)
+
+    # Increase the value of chance by 1
+    chances += 1
 
 
-    
-    
-
-    
-
-
+# Check whether the user guessed the correct number
+if not chances < 5:
+    print("YOU LOSE!!! The number is", number)
